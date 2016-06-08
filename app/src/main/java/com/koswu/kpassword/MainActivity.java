e62f551dfe8a6570f881ba05a8956597809295bb
KosWu <ws00298046@163.com>
2016-06-08 20:45:18 +0800

    加入关于和直达链接

--- app/src/main/java/com/koswu/kpassword/MainActivity.java
+++ app/src/main/java/com/koswu/kpassword/MainActivity.java
@@ -13,6 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.koswu.kpassword.MainActivity;
 import com.koswu.kpassword.tool.PassWord;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.AdapterView.OnItemClickListener;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -83,5 +91,50 @@ public class MainActivity extends AppCompatActivity
                 
             
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // TODO: Implement this method
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // TODO: Implement this method
+        switch (item.getItemId())
+        {
+            case R.id.item_about:
+                AlertDialog.Builder dialog_builder=new AlertDialog.Builder(this);
+                dialog_builder.setTitle(R.string.about);
+                dialog_builder.setMessage(R.string.about_message);
+                dialog_builder.setPositiveButton(R.string.ok,new DialogInterface.OnClickListener()
+                    {
+
+                        @Override
+                        public void onClick(DialogInterface dialog,int which)
+                        {
+                            // TODO: Implement this method
+                            dialog.dismiss();
+                        }
+                });
+                dialog_builder.create().show();
+            break;
+            case R.id.item_goto_web_version:
+                Intent intent_web_version=new Intent(Intent.ACTION_VIEW);
+                intent_web_version.setData(Uri.parse("http://koswu.github.io/K_PassWord_JS/"));
+                startActivity(intent_web_version);
+            break;
+            case R.id.item_goto_project_page:
+                Intent intent_project_page=new Intent(Intent.ACTION_VIEW);
+                intent_project_page.setData(Uri.parse("https://github.com/Koswu/K_PassWord"));
+                startActivity(intent_project_page);
+            default:
+                super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
